52e6d6fc34b52a0cc86decfc53ff32f06f248d6b
florije4ex <florije4ex@gmail.com>
2019-11-01 18:56:16 +0800

    Update: django cannot debug, so change to 2.1.13

--- django_graphql_movies/urls.py
+++ django_graphql_movies/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from movies.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False))),  # graphiql=True enable GraphiQL
 ]
